78ef7d4d6d6db74b8379ca58ee6c5cceb68fabb9
github username <github email address>
2017-06-19 11:05:42 -0400

    not done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  resources :appearances
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index', as: 'homepage'
   resources :guests, only: [:index]
+  get 'guests/:id', to: 'guests#show', as: 'guest'
   resources :episodes, only: [:index]
+  get 'episodes/:id', to: 'episodes#show', as: 'episode'
+  resources :users, except: [:new, :index]
+  get 'users/new', to: 'users#new', as: 'signup'
+  get 'sessions/new', to: 'sessions#new', as: 'login'
+  resources :sessions, except: [:new, :index, :show]
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
